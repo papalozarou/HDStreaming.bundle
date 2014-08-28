cd8a850efc7dfff9a7df343ab5685b9b700e6e08
papalozarou <loz@lab-7-mm.local>
2014-08-28 10:53:42 +0200

    Removed IP address, added vdn address

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -50,6 +50,8 @@ CHANNEL_IPS                     = ["5.63.145.149",
                                     "94.102.49.152",
                                     "146.185.28.197"]
                                     
+CHANNEL_PREFIX                  = "vdn.hd-streaming.tv"
+                                    
 # Global variable used to store channel list
 CHANNEL_LIST                    = []
 
@@ -321,7 +323,8 @@ def CreateChannelEpisodeObject(QUALITY,TITLE,SUMMARY,NUMBER,THUMB,INCLUDE_CONTAI
     
     # Builds a correctly formatted URL for each stream using a random IP number
     # from the CHANNEL_IPS array
-    VIDEO_URL           = VIDEO_PREFIX + random.choice(CHANNEL_IPS) + VIDEO_DIRECTORY + VIDEO_FILE
+    # VIDEO_URL           = VIDEO_PREFIX + random.choice(CHANNEL_IPS) + VIDEO_DIRECTORY + VIDEO_FILE
+    VIDEO_URL           = VIDEO_PREFIX + CHANNEL_PREFIX + VIDEO_DIRECTORY + VIDEO_FILE
     
     # Creates a VideoClipObject, with the key being a callback, unsure why, but
     # this re-calling of the same function is necessary to get an object that
